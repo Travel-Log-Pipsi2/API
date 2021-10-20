0f07cfc33f7e841fe36794af4dac11fbd7ed56e0
Mariusz Skuza <marsku8@wp.pl>
2021-10-20 20:08:34 +0200

    #added proper migration

--- Storage/DataAccessLayer/ApiDbContext.cs
+++ Storage/DataAccessLayer/ApiDbContext.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Storage.Models;
 using Storage.Models.Identity;
+using System;
 
 namespace Storage.DataAccessLayer
 {
-    public class ApiDbContext : DbContext
+    public class ApiDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public ApiDbContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Test> TestModels { get; set; }
-        public DbSet<User> Users { get; set; }
+        public override DbSet<User> Users { get; set; }
     }
 }
