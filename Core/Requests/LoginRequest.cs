7059142fe25de9d6d4cea5fc0a8f35aa44017fa7
Mariusz Skuza <marsku8@wp.pl>
2021-10-24 21:31:06 +0200

    change login logic

--- Core/Requests/LoginRequest.cs
+++ Core/Requests/LoginRequest.cs
@@ -4,8 +4,8 @@ namespace Core.Requests
 {
     public class LoginRequest
     {
-        [Required(ErrorMessage = "User Name is required")]
-        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
--- Core/Services/Authentication/LoginService.cs
+++ Core/Services/Authentication/LoginService.cs
@@ -4,6 +4,7 @@ using Core.Response;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Storage.Models.Identity;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Core.Services.Authentication
@@ -15,16 +16,16 @@ namespace Core.Services.Authentication
 
         public async Task<ServiceResponse> Login(LoginRequest model)
         {
-            var user = await _userManager.FindByNameAsync(model.Username);
+            var user = await _userManager.FindByEmailAsync(model.Email);
             if (await _userManager.CheckPasswordAsync(user, model.Password))
             {
                 var roles = await _userManager.GetRolesAsync(user);
                 var tokenResponse = _jwtGenerator.GenerateJWTToken(_config, user, roles);
 
-                return ServiceResponse<string>.Success($"{tokenResponse}", "Successful login");
+                return ServiceResponse<string>.Success(tokenResponse, "Successful login");
             }
 
-            return ServiceResponse.Error("Username or password is not correct!");
+            return ServiceResponse.Error("Email or password is not correct!");
         }
     }
 }
