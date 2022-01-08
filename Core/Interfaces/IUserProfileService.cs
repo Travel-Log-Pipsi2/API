20adfab979835e51708dba7a64798565abce1c3b
PiotrTekieli <piotr.tekieli@stud.pwsz.legnica.edu.pl>
2022-01-08 16:52:08 +0100

    user profile info and stats

---
+++ Core/Interfaces/IUserProfileService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Response;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IUserProfileService
+    {
+        public Task<ServiceResponse> GetInfo();
+        public Task<ServiceResponse> GetInfo(Guid userId);
+        public Task<ServiceResponse> GetStats();
+        public Task<ServiceResponse> GetStats(Guid userId);
+
+    }
+}
