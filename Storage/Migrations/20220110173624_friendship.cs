aaf9f19674654be53e91436e1e64e0cc2421884a
Mariusz Skuza <marsku8@wp.pl>
2022-01-10 19:06:20 +0100

    added model to database

---
+++ Storage/Migrations/20220110173624_friendship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class friendship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "frienships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromFriend = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ToFriend = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Notification = table.Column<bool>(type: "bit", nullable: false),
+                    IsAccepted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_frienships", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "frienships");
+        }
+    }
+}
