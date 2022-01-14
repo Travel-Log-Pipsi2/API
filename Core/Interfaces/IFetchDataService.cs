aa1b486aab63467b42c811f30b8113946c55ed13
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-01-15 00:54:45 +0100

    Deploy (#25)
    * Aded conroller, service, JSONHelper,
    adding travels from Graph API
    TODO
    storing ussers IdProvider and accesTokens
    
    * Deploy settings

---
+++ Core/Interfaces/IFetchDataService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Response;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IFetchDataService
+    {
+        public Task<ServiceResponse> Facebook();
+    }
+}
